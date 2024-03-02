2f1cf0c07ef3b662797ba92d6e5efba9febeeb4a
Comfort Ezengwa <111279380+comfortezengwa@users.noreply.github.com>
2024-03-02 14:03:10 +0000

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Comfort Ezengwa",
+  title: "Product designer",
+  email: "comfortezengwaa@gmail.com",
+  gitHub: "comfortezengwa",
+  instagram: "ihuomaaaaa",
+  linkedIn: "Comfort Ezengwa",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "ComfortEzengwa",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
